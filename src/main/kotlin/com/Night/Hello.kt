c15a526625f223fe55dd16c4221a17f478e4fe0d
Nightt1106 <40443125@gm.nfu.edu.tw>
2019-01-01 00:16:27 +0800

    1

---
+++ src/main/kotlin/com/Night/Hello.kt
@@ -0,0 +1,14 @@
+package com.Night
+
+fun main(args: Array<String>) {
+//    print("Hello ")
+//    Human().hello();
+    val h = Human();
+    h.hello();
+}
+
+class  Human{
+    fun hello(){
+        println("Hello Kotlin");
+    }
+}
